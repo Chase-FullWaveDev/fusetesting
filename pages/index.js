f94d4be08f59884ea1162658bd7ea09a7822c9f0
Chase-FullWaveDev <chase.fullwavedev@gmail.com>
2024-07-05 11:41:41 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,4 @@
-/*import memberstackDOM from '@memberstack';
+import memberstackDOM from '@memberstack/dom';
 const memberstack = memberstackDOM.init({
-    publicKey: "pk_sb_df3cce50a7e66ccd679b",
-});*/
-
-<script data-memberstack-app="app_cly8qbp6z01km0ts5bagz7mhv" src="https://static.memberstack.com/scripts/v1/memberstack.js" type="text/javascript"></script>
+  publicKey: "pk_6fcb0416c609e4891cda",
+});
