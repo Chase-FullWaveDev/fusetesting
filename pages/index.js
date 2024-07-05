aa070f7b20dc951c8a326d07170eea012a29a3ec
Chase-FullWaveDev <chase.fullwavedev@gmail.com>
2024-07-05 10:30:03 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,16 @@
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 */
+import memberstackDOM from '@memberstack/dom';
+
 export default function Home() {
-  return (
+  return
     <div className="container">
-    <Header>
-      import memberstackDOM from '@memberstack/dom';
-        const memberstack = memberstackDOM.init({
-          publicKey: "pk_sb_df3cce50a7e66ccd679b",
-        });
-  </Header>
+      <Header>
+          const memberstack = memberstackDOM.init({
+            publicKey: "pk_sb_df3cce50a7e66ccd679b",
+          });
+    </Header>
      /* <Head>
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
@@ -25,5 +26,4 @@ export default function Home() {
 
       <Footer /> */
     </div>
-  )
 }
