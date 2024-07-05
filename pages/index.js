6bccc19cfec4f06db5feac2f2a1d09b53be48b2a
Chase-FullWaveDev <chase.fullwavedev@gmail.com>
2024-07-05 11:35:56 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import memberstackDOM from '@memberstack/dom';
+import memberstackDOM from '@memberstack';
 const memberstack = memberstackDOM.init({
     publicKey: "pk_sb_df3cce50a7e66ccd679b",
 });
