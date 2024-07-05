2b36396461e196ef3eb70efe6efb7bd8b63c6ec0
Chase-FullWaveDev <chase.fullwavedev@gmail.com>
2024-07-05 10:38:42 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,13 @@
-/*import Head from 'next/head'
+/*import Head from 'next/head'*/
 import Header from '@components/Header'
 import Footer from '@components/Footer'
-*/
 import memberstackDOM from '@memberstack/dom';
 
 export default function Home() {
-  return
     <div className="container">
       <Header>
           const memberstack = memberstackDOM.init({
-            publicKey: "pk_sb_df3cce50a7e66ccd679b",
+            publicKey= "pk_sb_df3cce50a7e66ccd679b",
           });
     </Header>
      /* <Head>
