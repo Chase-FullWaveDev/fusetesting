e00e352b3399eaa787a1bb32228e9d922249c7a0
Chase-FullWaveDev <chase.fullwavedev@gmail.com>
2024-07-05 11:31:43 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,4 @@
-/*import Head from 'next/head'*/
-import Header from '@components/Header'
-import Footer from '@components/Footer'
 import memberstackDOM from '@memberstack/dom';
 const memberstack = memberstackDOM.init({
     publicKey: "pk_sb_df3cce50a7e66ccd679b",
 });
-
-export default function Home() {
-    <div className="container">
-      <Head>
-          
-    </Head>
-     /* <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer /> */
-    </div>
-}
